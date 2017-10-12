7ba2a40d71258521e5784aa58781e1f8e06835c6
Aaya-Hassan <abdulazeeznaji@yahoo.com>
2017-10-12 22:08:10 +0300

    add two methods to set data to local storage

--- subjects/subjects.js
+++ subjects/subjects.js
@@ -1,4 +1,18 @@
 angular.module('MyApp.subjects', [])
-.controller('subjectsCT',function ($scope,$location) {
-	$scope.data = {name:'azoz',time:'30'};
+.controller('subjectsCT',function ($scope,$location, $window) {
+	$scope.subjects = JSON.parse($window.localStorage.getItem('subjects'))
+	$scope.myValue = false
+	$scope.data = {}
+	$scope.addSubjects = function(){
+		$scope.myValue = false		
+	}
+	$scope.submit = function(){
+		var subjects = [];
+		if(JSON.parse($window.localStorage.getItem('subjects')) !== undefined){
+			subjects = JSON.parse($window.localStorage.getItem('subjects'))
+		}
+		subjects.push($scope.data) 
+		$window.localStorage.setItem("subjects", JSON.stringify(subjects))
+		$window.location.reload();
+	}
 })
